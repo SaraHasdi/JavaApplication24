f701abceefc3b4e2c55ba951fe0007affa91e576
sara <sara@192.168.3.41>
2016-12-02 11:53:46 +0000

    version 1

---
+++ src/javaapplication24/Calculation.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication24;
+
+/**
+ *
+ * @author sara
+ */
+
+public class Calculation {
+    public int calculer (int a, int b ){
+    return a+b;
+    }
+    
+}
